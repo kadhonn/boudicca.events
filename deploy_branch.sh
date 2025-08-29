23a322e29b65b35b96ced941d2adade48dfb3286
Matthias Ableidinger <ableimat@gmx.at>
2025-08-29 22:00:40 +0200

    Update deploy_branch.sh

--- deploy_branch.sh
+++ deploy_branch.sh
@@ -3,12 +3,12 @@
 branch="$1"
 cleanedBranch=`echo $branch | sed 's/[^a-zA-Z]*//g' | tr '[:upper:]' '[:lower:]'`;
 
-imageNamePublisher="ghcr.io/boudicca-events/publisher-event-html:branchdeployer-$cleanedBranch"
+imageNamePublisher="ghcr.io/kadhonn/publisher-event-html:branchdeployer-$cleanedBranch"
 echo "pushing $imageNamePublisher"
 docker tag localhost/boudicca-events-publisher-event-html "$imageNamePublisher"
 docker push "$imageNamePublisher"
 
-imageNameSearch="ghcr.io/boudicca-events/search:branchdeployer-$cleanedBranch"
+imageNameSearch="ghcr.io/kadhonn/search:branchdeployer-$cleanedBranch"
 echo "pushing $imageNamePublisher"
 docker tag localhost/boudicca-search "$imageNamePublisher"
 docker push "$imageNamePublisher"
